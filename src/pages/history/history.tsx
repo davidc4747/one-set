5aa2a4d9f4de47d6a23acea371fea27801d2c5a7
David Chung <davidc4747@yahoo.com>
2023-05-08 17:08:47 -0700

    Use moment to format dates

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -1,3 +1,4 @@
+import moment from "moment";
 import { Link } from "react-router-dom";
 import { Exercise } from "../../utils/exerciseService";
 
@@ -12,6 +13,18 @@ interface PropTypes {
 
 export default function History(props: PropTypes): React.ReactElement {
     const { history, clearHistory } = props;
+    const dateFormat = {
+        sameDay: "[Today]",
+        lastDay: "[Yesterday]",
+        lastWeek: "dddd",
+        sameElse: function (this: moment.Moment) {
+            if (this.isSame(Date.now(), "year")) {
+                return "MMM D";
+            } else {
+                return "MMM D YYYY";
+            }
+        },
+    };
 
     return (
         <main className="flex flex-col gap-lg">
@@ -29,15 +42,14 @@ export default function History(props: PropTypes): React.ReactElement {
                         key={index}
                         className="bg-primary-500 text-gray-400 px-lg py-sm  cursor-default first:rounded-t-lg last:rounded-b-lg flex justify-between hover:bg-primary-900 hover:text-white"
                     >
-                        <button
-                            className="bg-white border-2 border-red"
-                            onClick={() =>
-                                console.log(exercise.datetime.toJSON())
-                            }
-                        ></button>
                         <span>
-                            ({exercise.datetime.toDateString()}) {exercise.name}
+                            {moment(exercise.datetime).calendar(dateFormat)}
                         </span>
+                        {!moment(exercise.datetime).isSame(
+                            Date.now(),
+                            "month"
+                        ) && <span>{moment(exercise.datetime).fromNow()}</span>}
+                        <span>{exercise.name}</span>
                         <span>
                             {exercise.set}x{exercise.weight}lbs
                         </span>
